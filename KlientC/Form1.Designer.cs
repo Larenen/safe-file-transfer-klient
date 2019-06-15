03b3537c4c30037e945bb77410d5018cef05a9ab
larenen <luki.lary@gmail.com>
2019-06-16 00:45:21 +0200

    Initial Commit, got working connection to server, sending auth data, reading back list of user and displaying it

---
+++ KlientC/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace KlientC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbServerIp = new System.Windows.Forms.TextBox();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(187, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Połącz";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // tbServerIp
+            // 
+            this.tbServerIp.Location = new System.Drawing.Point(12, 12);
+            this.tbServerIp.Name = "tbServerIp";
+            this.tbServerIp.Size = new System.Drawing.Size(98, 22);
+            this.tbServerIp.TabIndex = 1;
+            this.tbServerIp.Text = "192.168.0.235";
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(116, 13);
+            this.numPort.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numPort.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(65, 22);
+            this.numPort.TabIndex = 2;
+            this.numPort.Value = new decimal(new int[] {
+            8080,
+            0,
+            0,
+            0});
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.ItemHeight = 16;
+            this.listBoxUsers.Location = new System.Drawing.Point(12, 41);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(250, 164);
+            this.listBoxUsers.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxUsers);
+            this.Controls.Add(this.numPort);
+            this.Controls.Add(this.tbServerIp);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbServerIp;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.ListBox listBoxUsers;
+    }
+}
+
---
+++ KlientC/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace KlientC
+{
+    class AuthStruct
+    {
+        public int RequestCode;
+        public string Username;
+        public string PublicKey;
+    }
+
+    class UserList
+    {
+        public int ReturnCode;
+        public string Message;
+        public List<User> Users;
+    }
+
+    class User
+    {
+        public string Username { get; set; }
+        public string PublicKey { get; set; }
+
+        public override string ToString()
+        {
+            return Username + ':' + PublicKey;
+        }
+    };
+
+    public partial class Form1 : Form
+    {
+        private readonly TcpClient _tcpClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _tcpClient = new TcpClient();
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            AuthStruct authStruct = new AuthStruct
+            {
+                RequestCode = 100,
+                PublicKey = "TestKey",
+                Username = "TestUsername"
+            };
+
+            _tcpClient.Connect(tbServerIp.Text,Convert.ToInt32(numPort.Value));
+
+            string initiateConnection = JsonConvert.SerializeObject(authStruct);
+
+            byte[] dataBytes = Encoding.Default.GetBytes(initiateConnection);
+
+            _tcpClient.GetStream().Write(dataBytes,0,dataBytes.Length);
+
+            while (_tcpClient.Available <= 0 && _tcpClient.Connected)
+            {
+                //tutaj dodac cos zeby nie czekal w kolko jak pajac
+                //wrzucic do innego watku
+            }
+
+            byte[] buffor = new byte[_tcpClient.Available];
+            _tcpClient.Client.Receive(buffor);
+
+            string jsonUserList = Encoding.Default.GetString(buffor);
+
+            UserList userList = JsonConvert.DeserializeObject<UserList>(jsonUserList);
+            if (userList.ReturnCode == 101)
+            {
+                listBoxUsers.Items.AddRange(userList.Users.ToArray());
+            }
+            else if(userList.ReturnCode == 102)
+            {
+                MessageBox.Show(userList.Message, "Błąd połączenia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
